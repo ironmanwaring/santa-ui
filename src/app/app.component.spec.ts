aa2749b549e8c4c8804ef2a12eda8ca594cd61c7
Phillip Manwaring <phillip.manwaring@gmail.com>
2016-12-26 21:03:39 -0500

    fix unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,48 +1,44 @@
-// /* tslint:disable:no-unused-variable */
+/* tslint:disable:no-unused-variable */
 
-// import { TestBed, async } from '@angular/core/testing';
-// import { RouterTestingModule } from '@angular/router/testing';
-// import { AppComponent } from './app.component';
-// import { SharedModule } from './shared/shared.module';
-// import { UsersModule } from './users/users.module';
-// import { AppRoutingModule } from './app-routing.module';
+import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { UsersModule } from './users/users.module';
 
-// describe('AppComponent', () => {
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [
-//         AppComponent
-//       ],
-//       imports: [
-//         SharedModule,
-//         UsersModule,
-//         RouterTestingModule.withRoutes([
-//           { path: 'users', }
-//         ])
-//       ],
-//       providers: [
+describe('AppComponent', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+      imports: [
+        SharedModule,
+        UsersModule,
+        RouterTestingModule.withRoutes([
+          { path: 'users', redirectTo: 'users' }
+        ])
+      ]
+    });
+    TestBed.compileComponents();
+  });
 
-//       ]
-//     });
-//     TestBed.compileComponents();
-//   });
+  it('should create the app', async(() => {
+    let fixture = TestBed.createComponent(AppComponent);
+    let app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  }));
 
-//   it('should create the app', async(() => {
-//     let fixture = TestBed.createComponent(AppComponent);
-//     let app = fixture.debugElement.componentInstance;
-//     expect(app).toBeTruthy();
-//   }));
+  it(`should have as title 'app works!'`, async(() => {
+    let fixture = TestBed.createComponent(AppComponent);
+    let app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('app works!');
+  }));
 
-//   it(`should have as title 'app works!'`, async(() => {
-//     let fixture = TestBed.createComponent(AppComponent);
-//     let app = fixture.debugElement.componentInstance;
-//     expect(app.title).toEqual('app works!');
-//   }));
-
-//   it('should render title in a h1 tag', async(() => {
-//     let fixture = TestBed.createComponent(AppComponent);
-//     fixture.detectChanges();
-//     let compiled = fixture.debugElement.nativeElement;
-//     expect(compiled.querySelector('h1').textContent).toContain('app works!');
-//   }));
-// });
+  it('should render title in a h1 tag', async(() => {
+    let fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    let compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+  }));
+});
