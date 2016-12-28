4b57d07ab2db2e054329336f19ce0cc35967714c
Stephanie Manwaring <stephanie.manwaring@gmail.com>
2016-12-27 21:35:55 -0500

    added login and logout functionality

---
+++ src/app/users/login/login.component.spec.ts
@@ -0,0 +1,33 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { LoginComponent } from './login.component';
+import { SharedModule } from '../../shared/shared.module';
+
+
+describe('LoginComponent', () => {
+  let component: LoginComponent;
+  let fixture: ComponentFixture<LoginComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ LoginComponent ],
+      imports: [
+        SharedModule,
+      ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LoginComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/users/users-routing.module.ts
+++ src/app/users/users-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { UsersComponent } from './users.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  { path: 'users', component:  UsersComponent},
+  { path: 'users', component:  UsersComponent },
+  { path: 'users/login', component: LoginComponent },
 ];
 
 @NgModule({
