589ae9e1396faf47437e7a697ee2f08d332c0fd1
Phillip Manwaring <phillip.manwaring@gmail.com>
2017-01-01 20:12:57 -0500

    fix failing test

--- src/app/shared/toolbar/toolbar.component.spec.ts
+++ src/app/shared/toolbar/toolbar.component.spec.ts
@@ -2,6 +2,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { ToolbarComponent } from './toolbar.component';
 import { AuthService } from '../auth/auth.service';
@@ -18,6 +19,7 @@ describe('ToolbarComponent', () => {
       declarations: [ ToolbarComponent ],
       imports: [
         MaterialModule.forRoot(),
+        RouterTestingModule
       ],
       providers: [
         AuthService,
