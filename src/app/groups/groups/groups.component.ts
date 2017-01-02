c191f1909e5559a51d866cdd362921485fbeae2c
Phillip Manwaring <phillip.manwaring@gmail.com>
2017-01-01 20:36:43 -0500

    add group icon

--- src/app/groups/groups/groups.component.ts
+++ src/app/groups/groups/groups.component.ts
@@ -1,23 +1,30 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { MdDialog } from '@angular/material';
 import { GroupService } from '../shared/group.service';
 import { Group } from '../shared/group';
 
 @Component({
   selector: 'app-groups',
   templateUrl: './groups.component.html',
-  styleUrls: ['./groups.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./groups.component.scss']
 })
 export class GroupsComponent implements OnInit {
+  
   groups: Group[] = [];
+  showAddGroup: boolean = false;
   
   constructor(
-    private groupService: GroupService
+    private groupService: GroupService,
+    public dialog: MdDialog
   ) { }
 
-   // requests group lists of users groups
   ngOnInit () {
     this.groups = this.groupService.getAll();
+  }
+
+  public showGroup(): void {
+    this.showAddGroup = true;
+    // this.dialog.open(AddGroupDialog);
   }
 
 }
