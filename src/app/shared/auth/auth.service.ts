971878c796c8e1d03423ea3e8686b7d9a4729d3f
Phillip Manwaring <phillip.manwaring@gmail.com>
2017-01-01 20:04:31 -0500

    route to groups after login

--- src/app/shared/auth/auth.service.ts
+++ src/app/shared/auth/auth.service.ts
@@ -1,67 +1,65 @@
 import { Injectable } from '@angular/core';
 import { tokenNotExpired } from 'angular2-jwt';
+import { Router } from '@angular/router';
 
-// Avoid name not found warnings
 let Auth0Lock = require('auth0-lock').default;
 
 @Injectable()
 export class AuthService {
 
-  // Configure Auth0
   lock = new Auth0Lock('e0VgaUxRSIvPUVOy5Sx5rkgAdeN5rzja', 'santaswap.auth0.com', {});
 
- //Store profile object in auth class
-  userProfile: Object;
+  userProfile: any;
 
-  constructor() {
-    // Set userProfile attribute of already saved profile
-    this.userProfile = JSON.parse(localStorage.getItem('profile'));
+  constructor(
+    private router: Router
+  ) {
+    this.initAuthentication();
+  }
 
-    // Add callback for lock `authenticated` event
-    this.lock.on("authenticated", (authResult) => {
-      localStorage.setItem('id_token', authResult.idToken);
+  public login(): void {
+    this.lock.show();
+  }
 
-      // Fetch profile information
-      this.lock.getProfile(authResult.idToken, (error, profile) => {
+  public logout(): void {
+    localStorage.removeItem('id_token');
+    localStorage.removeItem('profile');
+    this.userProfile = null;
+    this.redirectToHome();
+  }
+
+  public authenticated(): boolean {
+    return tokenNotExpired();
+  }
+
+  public user(): any {
+    return this.userProfile
+  }
+
+  public initAuthentication(): void {
+    this.lock.on('authenticated', (authResult) => {
+      this.lock.getUserInfo(authResult.accessToken, (error, profile) => {
         if (error) {
-          // Handle error
-          alert(error);
-          return;
+          console.error(error);
+        } else {
+          this.cacheAuthResult(authResult, profile);
         }
-        localStorage.setItem('profile', JSON.stringify(profile));
-        this.userProfile = profile;
-        console.log(profile);
       });
     });
   }
 
-  public login() {
-    // Call the show method to display the widget.
-    this.lock.show();
+  public cacheAuthResult(authResult: any, profile: any): void {
+    localStorage.setItem('id_token', authResult.idToken);
+    localStorage.setItem('profile', JSON.stringify(profile));
+    this.userProfile = profile;
+    this.redirectToGroups();
   }
 
-  public authenticated() {
-    // Check if there's an unexpired JWT
-    // This searches for an item in localStorage with key == 'id_token'
-    return tokenNotExpired();
+  public redirectToGroups(): void {
+    this.router.navigate(['/groups']);
   }
 
-  public logout() {
-    // Remove token from localStorage
-    console.log("oh hi");
-    localStorage.removeItem('id_token');
-    localStorage.removeItem('profile');
-    this.userProfile = null;
+  public redirectToHome(): void {
+    this.router.navigate(['/']);
   }
-
-  public user() {
-    return this.userProfile
-  }
-
 }
-
-
-
-
-
-
