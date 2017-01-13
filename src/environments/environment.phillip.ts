0542ac371adfe44e3da72ad9231733aded1e4d93
Phillip Manwaring <phillip.manwaring@gmail.com>
2017-01-12 22:15:12 -0500

    add phillip and stephanie environment variables

---
+++ src/environments/environment.phillip.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  groupsEndpoint: 'https://cujys22c3i.execute-api.us-east-1.amazonaws.com/Phillip',
+  usersEndpoint: 'https://4gx60vi2a4.execute-api.us-east-1.amazonaws.com/Phillip'
+};
