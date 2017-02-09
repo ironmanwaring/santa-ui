7ea94aad868ac3a601a937210c0bf71bb2b880be
Phillip Manwaring <phillip.manwaring@gmail.com>
2017-02-09 16:46:08 -0500

    lowercasing stage for bucket name validation

--- infrastructure/travis-deploy.sh
+++ infrastructure/travis-deploy.sh
@@ -2,7 +2,7 @@
 set -e
 
 AUTHOR_NAME=$(git log -1 $TRAVIS_COMMIT --pretty="%aN")
-FIRST_NAME=${AUTHOR_NAME%% *}
+FIRST_NAME=${AUTHOR_NAME%%,, *}
 echo Author is $AUTHOR_NAME, first name is $FIRST_NAME
 
 if [ $TRAVIS_PULL_REQUEST != "false" ]; then
@@ -10,7 +10,7 @@ if [ $TRAVIS_PULL_REQUEST != "false" ]; then
 
 else
     echo Deploying serverless project with code from branch: $TRAVIS_BRANCH
-    [[ $TRAVIS_BRANCH = "master" ]] && STAGE="Prod" || STAGE=$FIRST_NAME
+    [[ $TRAVIS_BRANCH = "master" ]] && STAGE="prod" || STAGE=$FIRST_NAME
     echo cd infrastructure \&\& serverless deploy --stage $STAGE
     cd infrastructure && serverless deploy --stage $STAGE
 
