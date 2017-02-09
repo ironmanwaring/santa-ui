89cf64e57ce638fd3925a9c5a1275e5103b1f5d7
Phillip Manwaring <phillip.manwaring@gmail.com>
2017-02-08 23:56:23 -0500

    update deployment location

--- infrastructure/travis-deploy.sh
+++ infrastructure/travis-deploy.sh
@@ -11,7 +11,7 @@ if [ $TRAVIS_PULL_REQUEST != "false" ]; then
 else
     echo Deploying serverless project with code from branch: $TRAVIS_BRANCH
     [[ $TRAVIS_BRANCH = "master" ]] && STAGE="Prod" || STAGE=$FIRST_NAME
-    echo serverless deploy --stage $STAGE
-    serverless deploy --stage $STAGE
+    echo cd infrastructure && serverless deploy --stage $STAGE
+    cd infrastructure && serverless deploy --stage $STAGE
 
 fi
