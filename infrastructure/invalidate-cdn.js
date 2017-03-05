1c1fff9b4c37552005ea88c69b4ab274781b0f6e
Phillip Manwaring <phillip.manwaring@gmail.com>
2017-03-04 19:18:19 -0500

    workaround for serverless s3 event configuration

--- infrastructure/invalidate-cdn.js
+++ infrastructure/invalidate-cdn.js
@@ -3,6 +3,7 @@ const AWS = require('aws-sdk');
 
 module.exports.handler = (event, context, callback) => {
   console.log('Received event to invalidate cdn: ', event);
+  console.log('Stringified', JSON.stringify(event));
   callback(null, 'Received event');
   
 };
