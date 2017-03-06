2debe0a5210e3e90ed1dc75c0fefe78af89a6f76
Phillip Manwaring <phillip.manwaring@gmail.com>
2017-03-06 18:48:09 -0500

    update travis deploy script

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -2,15 +2,26 @@
 set -e
 
 if [ $TRAVIS_PULL_REQUEST != "false" ]; then
+    
     echo Not deploying serverless code on pull requests
 
 else
+    
     echo Deploying serverless project with code from branch: $TRAVIS_BRANCH
     
     echo cd infrastructure \&\& serverless deploy --stage $STAGE
     cd infrastructure && serverless deploy --stage $STAGE
 
-    echo ng build --env=$STAGE
-    ng build --env=$STAGE --output-hashing=all
+    if [ $STAGE == "prod" ]; then
 
+        echo ng build --prod --output-hashing=all
+        ng build --prod --output-hashing=all
+
+    else
+
+        echo ng build --env=$STAGE
+        ng build --env=$STAGE --output-hashing=all
+
+    fi
+    
 fi
