cee8d0a21543a548ef742310385496deedd463b5
Phillip Manwaring <phillip.manwaring@gmail.com>
2017-03-24 23:30:37 -0400

    test invalidate cdn with s3 events

--- infrastructure/invalidate-cdn.js
+++ infrastructure/invalidate-cdn.js
@@ -1,9 +1,26 @@
 'use strict';
 const AWS = require('aws-sdk');
+const cloudFront = new AWS.CloudFront({ apiVersion: '2016-11-25' })
 
 module.exports.handler = (event, context, callback) => {
   console.log('Received event to invalidate cdn: ', event);
   console.log('Stringified', JSON.stringify(event));
-  callback(null, 'Received event');
-  
+  callback(null, 'Worked?');
+  // invalidateCDN()
+  //   .then( results => callback(null, results))
+  //   .catch( err => callback(err, 'Unexpected error'));  
+};
+
+const invalidateCDN = () => {
+  const params = {
+    DistributionId: process.env.CDN,
+    InvalidationBatch: {
+      CallerReference: (new Date()).getTime(),
+      Paths: {
+        Quantity: 1,
+        Items: [ 'index.html' ]
+      }
+    }
+  };
+  return cloudFront.createInvalidation(params).promise();
 };
