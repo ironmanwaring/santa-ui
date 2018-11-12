0cf5d78c8146eb622cba0bda6343f44be77ef32c
Phillip Manwaring <phillip.manwaring@gmail.com>
2018-11-12 13:39:43 -0600

    add prettier

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,10 +20,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ui'`, () => {
+  it(`should have as title 'santa swap'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ui');
+    expect(app.title).toEqual('santa swap');
   });
 
   it('should render title in a h1 tag', () => {
