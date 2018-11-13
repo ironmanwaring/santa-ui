1ed82022c2b0488bbecddee5a841b35212941d10
Phillip Manwaring <phillip.manwaring@gmail.com>
2018-11-13 10:25:10 -0600

    clean up theming code

--- src/app/core/services/theme.service.ts
+++ src/app/core/services/theme.service.ts
@@ -1,3 +1,4 @@
+// Theme switching based on https://grensesnittet.computas.com/dynamic-themes-in-angular-material/
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
