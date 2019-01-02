27691b8d1f05606625e91d4e859fbab5d89074ee
Phillip <phillip.manwaring@gmail.com>
2019-01-02 01:44:07 -0500

    update group members pipe logic

--- src/app/group-card/group-members.pipe.ts
+++ src/app/group-card/group-members.pipe.ts
@@ -3,8 +3,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({ name: 'groupMembers' })
 export class GroupMembersPipe implements PipeTransform {
   transform(members: string[], maxLength: number = 40): any {
-    members = members.map(member => member.split(' ')[0]);
-    const display = members ? members.join(', ') : '';
+    const display = members ? members.map(member => member.split(' ')[0]).join(', ') : '';
     return display.length <= maxLength ? display : `${display.substring(0, maxLength)}...`;
   }
 }
