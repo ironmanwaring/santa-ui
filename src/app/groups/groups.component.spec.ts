be70c55b5ebf9ebeae7232f01019c4b7ad3c4ce2
Phillip <phillip.manwaring@gmail.com>
2019-01-10 23:31:20 -0500

    Update groups.component.spec.ts

--- src/app/groups/groups.component.spec.ts
+++ src/app/groups/groups.component.spec.ts
@@ -8,6 +8,7 @@ import { AuthService } from '../auth/auth.service';
 import { GroupMembersPipe } from '../group-card/group-members.pipe';
 import { CreateCardComponent } from '../create-card/create-card.component';
 import { JoinCardComponent } from '../join-card/join-card.component';
+import { GroupStatusPipe } from '../group-card/group-status.pipe';
 
 describe('GroupsComponent', () => {
   let component: GroupsComponent;
@@ -15,7 +16,14 @@ describe('GroupsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [GroupsComponent, GroupCardComponent, GroupMembersPipe, CreateCardComponent, JoinCardComponent],
+      declarations: [
+        GroupsComponent,
+        GroupCardComponent,
+        GroupMembersPipe,
+        GroupStatusPipe,
+        CreateCardComponent,
+        JoinCardComponent
+      ],
       imports: [RouterTestingModule, SharedModule],
       providers: [AuthService]
     }).compileComponents();
