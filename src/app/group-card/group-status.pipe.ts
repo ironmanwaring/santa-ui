3681f85a4d83c85eec8f6a5bb2a4ffc53b45564a
Phillip <phillip.manwaring@gmail.com>
2019-01-10 23:37:41 -0500

    update group status pipe verbage

--- src/app/group-card/group-status.pipe.ts
+++ src/app/group-card/group-status.pipe.ts
@@ -3,6 +3,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({ name: 'groupStatus' })
 export class GroupStatusPipe implements PipeTransform {
   transform(matched: boolean): any {
-    return matched ? 'This group has been matched' : 'Waiting for more members to join';
+    return matched ? 'This group has been matched' : 'Waiting for more people to join';
   }
 }
