b86565b841e723b51f9a8ecda34ad43df0394ba4
Phillip <phillip.manwaring@gmail.com>
2019-01-10 23:03:17 -0500

    update group details to change based on matching status

--- src/app/groups/group.ts
+++ src/app/groups/group.ts
@@ -5,12 +5,14 @@ export class Group {
   name: string;
   code: string;
   members: string[];
+  matched: boolean;
 }
 
 export class GroupDetail {
   groupId: string;
   name: string;
   code: string;
+  matched: boolean;
   members: Profile[];
   profile: ProfileDetail;
 }
