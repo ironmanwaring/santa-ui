b28304696995f0d28453aa5f7c3eeb1007becf0c
Phillip <phillip.manwaring@gmail.com>
2019-01-12 23:56:02 -0500

    add google analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth/auth.service';
 import { ThemeService } from './theme/theme.service';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -11,11 +12,24 @@ import { ThemeService } from './theme/theme.service';
 export class AppComponent implements OnInit {
   isDarkTheme: Observable<boolean>;
 
-  constructor(private theme: ThemeService, public auth: AuthService) {
+  constructor(private theme: ThemeService, public auth: AuthService, private router: Router) {
     auth.handleAuthentication();
   }
 
   ngOnInit() {
+    this.setTheme();
+    this.enableAnalytics();
+  }
+
+  enableAnalytics() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        (<any>window).gtag('config', 'UA-90417656-1', { page_path: event.urlAfterRedirects });
+      }
+    });
+  }
+
+  setTheme() {
     this.isDarkTheme = this.theme.isDarkTheme;
     if (localStorage.getItem('isLoggedIn') === 'true') {
       this.auth.renewTokens();
